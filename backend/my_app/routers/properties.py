5ff936af68cfbe8df3a8e2bae1ddca4c4ae5685b
sqreele <kkanthong@gmail.com>
2025-07-23 15:22:08 +0700

    Update properties.py

--- backend/my_app/routers/properties.py
+++ backend/my_app/routers/properties.py
@@ -7,7 +7,7 @@ from typing import List, Optional
 
 from database import get_db
 from models import User, UserRole
-from schema import (
+from schemas import (
     Property as PropertySchema, PropertyCreate, PropertyUpdate,
     Room as RoomSchema, RoomCreate, RoomUpdate,
     UserPropertyAccess as UserPropertyAccessSchema,
